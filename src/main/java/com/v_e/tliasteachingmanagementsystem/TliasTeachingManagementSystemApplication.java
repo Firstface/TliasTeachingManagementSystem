6556d19c6897b6166e2479b097eb685c17a6b047
V_E <E1391157@u.nus.edu>
2025-02-07 23:43:18 +0800

    Initial commit

---
+++ src/main/java/com/v_e/tliasteachingmanagementsystem/TliasTeachingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.v_e.tliasteachingmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasTeachingManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasTeachingManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/v_e/tliasteachingmanagementsystem/TliasTeachingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v_e.tliasteachingmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TliasTeachingManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
